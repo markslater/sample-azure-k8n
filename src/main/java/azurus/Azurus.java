0aa61b4782a36abc773e5c3b1e163f33704007a5
Mark Slater <mark.slater@mail.com>
2018-04-26 20:52:28 +0100

    Make it run a webserver, that can run until it's stopped.

--- src/main/java/azurus/Azurus.java
+++ src/main/java/azurus/Azurus.java
@@ -1,9 +1,32 @@
 package azurus;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
 public final class Azurus {
 
-	public static void main(final String... args) {
-		System.out.println("Hello, World!");
-	}
+    public static void main(final String... args) throws IOException {
+        System.out.println("Hello, World!");
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        httpServer.createContext("/", httpExchange -> {
+            httpExchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
+            httpExchange.sendResponseHeaders(200, 0);
+            try (final OutputStream outputStream = httpExchange.getResponseBody();
+                 final Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+                writer.write("Hello, World!");
+            }
+        });
+        httpServer.setExecutor(newFixedThreadPool(10));
+        httpServer.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> httpServer.stop(10)));
+    }
 
 }
