7345c2dc9f1c2a894d8bd2cf1389ddb525e46d5f
Mark Slater <mark.slater@mail.com>
2018-05-10 16:27:00 +0100

    Jang in some Cosmos writing?

--- src/main/java/azurus/Azurus.java
+++ src/main/java/azurus/Azurus.java
@@ -2,19 +2,29 @@ package azurus;
 
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public final class Azurus {
 
-    public static void main(final String... args) throws IOException {
+    public static void main(final String... args) throws Exception {
         System.out.println("Hello, World!");
+        try (
+                final InputStream inputStream = Files.newInputStream(Paths.get("etc", "cosmos-credentials", "master-key.txt"));
+                final InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            final String masterKey = org.apache.commons.io.IOUtils.toString(inputStreamReader);
+            System.out.println("Got a master key that was " + masterKey.length() + " characters long.");
+            try (final RequestStorage requestStorage = new RequestStorageService("foo", masterKey).start()) {
+                requestStorage.storeSomething();
+                System.out.println("Stored something...");
+            }
+        }
+
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
         httpServer.createContext("/", httpExchange -> {
             httpExchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
